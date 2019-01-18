148daec0599e4546f5a7c504786d3f89e141beaf
Nikos Dalezios <ndalezios@gmail.com>
2019-01-18 12:21:16 +0200

    argparse

--- playing.py
+++ playing.py
@@ -1,17 +1,20 @@
 import sys
 import json
+import argparse
 
 program_name = sys.argv[0]
 print(program_name)
 #arguments = sys.argv[1:]
 #print(arguments)
 
-if len(sys.argv) != 2 :
-	print("Usage: python3 playing.py input_file")
-	sys.exit(1)
-else :
-	print("Parsing file : %s" %(sys.argv[1]))
+parser = argparse.ArgumentParser() 
+parser.add_argument("-i","--input", required=True, 
+	help="input file containing json CADF event records")
+args = parser.parse_args()
+
+input_file = args.input
+print("Parsing file : %s" %(input_file))
 	
-with open(sys.argv[1], "r") as input_raw:
-    data = json.load(input_raw)
+with open(input_file, "r") as input_data:
+    data = json.load(input_data)
 print(json.dumps(data, indent=4))
