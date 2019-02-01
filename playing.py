202791a64b0af4970147b03c361b0593b7dc5612
nikos <ndalezios@gmail.com>
2019-02-01 12:54:04 +0200

    .

--- playing.py
+++ playing.py
@@ -1,9 +1,19 @@
 import sys
 import json
-from json.decoder import JSONDecodeError
+from json import JSONDecodeError
 import argparse
 
 
+def parse_file(infile):
+	print("Parsing file : %s" %(infile))
+	with open(infile, "r") as indata:
+		try:
+			outdata = json.load(indata)
+		except JSONDecodeError:
+			print("Not a valid JSON file")
+			outdata = []
+	return outdata
+    
 program_name = sys.argv[0]
 print(program_name)
 #arguments = sys.argv[1:]
@@ -15,30 +25,25 @@ parser.add_argument("-i","--input", required=True,
 args = parser.parse_args()
 
 input_file = args.input
-print("Parsing file : %s" %(input_file))
-	
-with open(input_file, "r") as input_data:
-    try:
-	    data = json.load(input_data)
-    except JSONDecodeError:
-	    print("Not a valid JSON file")
-		
-print(json.dumps(data, indent=4))
-
-print("==============================================================")
-print("Found %s CADF event records" %(len(data)))
-print("==============================================================")
-print(json.dumps(data[:1], indent=4))
-print("==============================================================")
-print("OBSERVER %s" %data[0]["observer"])
-print("--------------------------------------------------------------")
-print("WHO (initiator) %s" %data[0]["initiator"])
-print("--------------------------------------------------------------")
-print("TARGET %s" %data[0]["target"])
-print("--------------------------------------------------------------")
-print("EVENT_TYPE %s" %data[0]["eventType"])
-print("--------------------------------------------------------------")
-print("WHEN (eventTime UTC) %s" %data[0]["eventTime"])
+data = parse_file(input_file)
+if len(data) == 0 :
+	pass
+else:
+	#print(json.dumps(data, indent=4))
+	print("==============================================================")
+	print("Found %s CADF event records" %(len(data)))
+	print("==============================================================")
+	print(json.dumps(data[:1], indent=4))
+	print("==============================================================")
+	print("OBSERVER %s" %data[0]["observer"])
+	print("--------------------------------------------------------------")
+	print("WHO (initiator) %s" %data[0]["initiator"])
+	print("--------------------------------------------------------------")
+	print("TARGET %s" %data[0]["target"])
+	print("--------------------------------------------------------------")
+	print("EVENT_TYPE %s" %data[0]["eventType"])
+	print("--------------------------------------------------------------")
+	print("WHEN (eventTime UTC) %s" %data[0]["eventTime"])
 
 
 
