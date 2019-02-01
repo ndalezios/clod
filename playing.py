d0d3a0c008af265700adb36b79f9373d55e7e291
nikos <ndalezios@gmail.com>
2019-02-01 11:48:59 +0200

    exceptions

--- playing.py
+++ playing.py
@@ -18,3 +18,22 @@ print("Parsing file : %s" %(input_file))
 with open(input_file, "r") as input_data:
     data = json.load(input_data)
 print(json.dumps(data, indent=4))
+
+print("==============================================================")
+print("Found %s CADF event records" %(len(data)))
+print("==============================================================")
+print(json.dumps(data[:1], indent=4))
+print("==============================================================")
+print("OBSERVER %s" %data[0]["observer"])
+print("--------------------------------------------------------------")
+print("WHO (initiator) %s" %data[0]["initiator"])
+print("--------------------------------------------------------------")
+print("TARGET %s" %data[0]["target"])
+print("--------------------------------------------------------------")
+print("EVENT_TYPE %s" %data[0]["eventType"])
+print("--------------------------------------------------------------")
+print("WHEN (eventTime UTC) %s" %data[0]["eventTime"])
+
+
+
+
